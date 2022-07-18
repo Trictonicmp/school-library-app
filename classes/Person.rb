bc24014717e2be0f7522ac97291b3aeb42ad8524
Trictonicmp <code_shadow@hotmail.com>
2022-07-18 14:48:59 -0500

    Fix linter warnings/errors

--- classes/Person.rb
+++ classes/Person.rb
@@ -1,62 +1,46 @@
-class Person
-  @id
-  @name
-  @age
-  @parent_permission
+# frozen_string_literal: true
 
+# app/classes/person.rb
+class Person
   private
-  def is_of_age?
-    return @age >= 18
+
+  def of_age?
+    @age >= 18
   end
 
   public
-  def initialize(name = "Unknown", parent_permission = true, age)
+
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = 0
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
 
-  def get_id
-    @id
-  end
-
-  def get_name
-    @name
-  end
-
-  def get_age
-    @age
-  end
-
-  def set_name(name)
+  def name?(name)
     @name = name
   end
 
-  def set_age(age)
+  def age?(age)
     @age = age
   end
 
-  def set_can_use_services(can_use)
-    @can_use_services = can_use
-  end
-
   def can_use_services?
-    is_of_age? || @parent_permission
+    of_age? || @parent_permission
   end
 end
 
-=begin 
-person = Person.new("Amaury", false, 26)
-person.get_name
-person.get_age
-person.can_use_services?
-person.set_can_use_services(true)
-person.can_use_services?
-
-person.set_name("Mario")
-person.set_age(16)
-person.set_can_use_services(true)
-person.get_name
-person.get_age
-person.can_use_services? 
-=end
+#
+# person = Person.new("Amaury", false, 26)
+# person.get_name
+# person.get_age
+# person.can_use_services?
+# person.set_can_use_services(true)
+# person.can_use_services?
+#
+# person.set_name("Mario")
+# person.set_age(16)
+# person.set_can_use_services(true)
+# person.get_name
+# person.get_age
+# person.can_use_services?
