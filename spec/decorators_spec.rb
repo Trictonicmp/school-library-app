f9ff692637cd97c0aaaec119c19731eaa098bde5
Trictonicmp <code_shadow@hotmail.com>
2022-07-18 22:58:55 -0500

    Fix linter errors

--- spec/decorators_spec.rb
+++ spec/decorators_spec.rb
@@ -7,8 +7,8 @@ describe BaseDecorator do
   describe 'correct_name method' do
     it 'returns "John" name from person class' do
       person = Person.new(20, 'John', parent_permission: false)
-      baseDecorator = BaseDecorator.new(person)
-      expect(baseDecorator.correct_name).to eql('John')
+      base_decorator = BaseDecorator.new(person)
+      expect(base_decorator.correct_name).to eql('John')
     end
   end
 end
@@ -16,9 +16,9 @@ end
 describe CapitalizeDecorator do
   describe 'correct_name method from person "john"' do
     person = Person.new(20, 'john', parent_permission: false)
-    capitalizeDecorator = CapitalizeDecorator.new(person)
+    capitalize_decorator = CapitalizeDecorator.new(person)
     it 'returns John after capitalize decorator' do
-      expect(capitalizeDecorator.correct_name).to eql('John')
+      expect(capitalize_decorator.correct_name).to eql('John')
     end
   end
 end
@@ -27,8 +27,8 @@ describe TrimmerDecorator do
   describe 'correct_name method from person "Amaury Elohir"' do
     it 'returns "Amaury Elo"' do
       person = Person.new(20, 'Amaury Elohir', parent_permission: false)
-      trimmerDecorator = TrimmerDecorator.new(person)
-      expect(trimmerDecorator.correct_name).to eql('Amaury Elo')
+      trimmer_decorator = TrimmerDecorator.new(person)
+      expect(trimmer_decorator.correct_name).to eql('Amaury Elo')
     end
   end
 end
@@ -36,13 +36,13 @@ end
 describe 'Sample test from project requirements' do
   describe 'Decorators for person "maximilianus"' do
     person = Person.new(22, 'maximilianus')
-    capitalizedPerson = CapitalizeDecorator.new(person)
+    capitalized_person = CapitalizeDecorator.new(person)
     it 'returns Maximilianus' do
-      expect(capitalizedPerson.correct_name).to eql('Maximilianus')
+      expect(capitalized_person.correct_name).to eql('Maximilianus')
     end
-    capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
+    capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
     it 'returns "Maximilian"' do
-      expect(capitalizedTrimmedPerson.correct_name).to eql('Maximilian')
+      expect(capitalized_trimmed_person.correct_name).to eql('Maximilian')
     end
   end
 end
