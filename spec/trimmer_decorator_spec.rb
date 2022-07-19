62252457d2b1591ad23d43ebdc34e6b2b10df3d4
Trictonicmp <code_shadow@hotmail.com>
2022-07-18 22:37:05 -0500

    refactor decorators test to be in one file

---
+++ spec/trimmer_decorator_spec.rb
@@ -0,0 +1,12 @@
+#require_relative '../classes/trimmer_decorator'
+#require_relative '../classes/person'
+
+#describe TrimmerDecorator do
+#  describe 'correct_name method from person "Amaury Elohir"' do
+#    it 'returns "Amaury Elo"'
+#      person = Person.new(20, 'Amaury Elohir', parent_permission: false)
+#      trimmerDecorator = TrimmerDecorator.new(person)
+#      expect(trimmerDecorator.correct_name).to eql('Amaury Elo')
+#    end
+#  end
+#end
