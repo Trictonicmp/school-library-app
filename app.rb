2b6d1863ed0787cfead718a23b97459c68b4338b
Trictonicmp <code_shadow@hotmail.com>
2022-07-20 13:38:13 -0500

    Fix linter errors

--- app.rb
+++ app.rb
@@ -57,14 +57,14 @@ class App
     age = 0
     name = ''
     parent_permission = false
-    until_data_ok {
+    until_data_ok do
       print 'Age: '
       age = gets.chomp
       print 'Name: '
       name = gets.chomp
       print 'Has parent permission? [Y/N]'
       parent_permission = parse_response(gets.chomp)
-    }
+    end
     Student.new(@classroom, age, name, parent_permission: parent_permission)
   end
 
@@ -72,14 +72,14 @@ class App
     age = 0
     name = ''
     specialization = ''
-    until_data_ok {
+    until_data_ok do
       print 'Age: '
       age = gets.chomp
       print 'Name: '
       name = gets.chomp
       print 'Specialization: '
       specialization = gets.chomp
-    }
+    end
     Teacher.new(specialization, age, name, parent_permission: true)
   end
 
@@ -110,12 +110,12 @@ class App
   def create_book
     title = ''
     author = ''
-    until_data_ok {
+    until_data_ok do
       print 'Title: '
       title = gets.chomp
       print 'Author: '
       author = gets.chomp
-    }
+    end
     Book.new(title, author)
   end
 
--- helpers/helpers.rb
+++ helpers/helpers.rb
@@ -1,3 +1,4 @@
+# app/helpers/helpers.rb
 def parse_response(response)
   is_data_ok = false if %w[n N].include? response
   is_data_ok = true if %w[y Y].include? response
