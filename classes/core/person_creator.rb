67db8c36c3d54020a4d582a479c611e4657ae4cc
Trictonicmp <code_shadow@hotmail.com>
2022-07-25 19:53:25 -0500

    Change: Refactor PersonCreator class

--- classes/core/person_creator.rb
+++ classes/core/person_creator.rb
@@ -2,33 +2,50 @@ require_relative '../../helpers/helpers'
 
 # app/classes/core/person_creator.rb
 class PersonCreator
-  def create_student
+  private
+  def ask_student_info
     age = 0
     name = ''
     parent_permission = false
     until_data_ok do
-      print 'Age: '
-      age = gets.chomp
-      print 'Name: '
-      name = gets.chomp
+      age, name = ask_common_data
       print 'Has parent permission? [Y/N]: '
       parent_permission = parse_response(gets.chomp)
     end
-    Student.new(@classroom, age, name, parent_permission: parent_permission)
+
+    return age, name, parent_permission
   end
 
-  def create_teacher
+  def ask_teacher_info
     age = 0
     name = ''
     specialization = ''
     until_data_ok do
-      print 'Age: '
-      age = gets.chomp
-      print 'Name: '
-      name = gets.chomp
+      age, name = ask_common_data
       print 'Specialization: '
       specialization = gets.chomp
     end
+
+    return age, name, specialization
+  end
+
+  def ask_common_data
+    print 'Age: '
+    age = gets.chomp
+    print 'Name: '
+    name = gets.chomp
+
+    return age, name
+  end
+
+  public
+  def create_student
+    age, name, parent_permission = ask_student_info 
+    Student.new(@classroom, age, name, parent_permission: parent_permission)
+  end
+
+  def create_teacher
+    age, name, specialization = ask_teacher_info
     Teacher.new(specialization, age, name, parent_permission: true)
   end
 end
