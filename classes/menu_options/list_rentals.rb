93d983224f033cd635e87651ef797a9a825a2615
Trictonicmp <code_shadow@hotmail.com>
2022-07-26 01:26:01 -0500

    Add ListRentals class

---
+++ classes/menu_options/list_rentals.rb
@@ -0,0 +1,30 @@
+require_relative '../core/persons_lister'
+require_relative '../helpers/helpers'
+
+# app/classes/menu_options
+class ListRentals
+  def initialize(persons_list)
+    @persons_list = persons_list
+    @persons_lister = PersonLister.new(persons_list)
+  end
+
+  def list_rentals
+    @persons_lister.list_persons
+    print "\n"
+    print 'Person ID: '
+    id = Integer(gets.chomp)
+    choosen_person = nil
+    puts 'Rentals: '
+    @state.persons.each do |person|
+      choosen_person = person if person.id == id
+    end
+    return if choosen_person.nil?
+
+    choosen_person.rentals.each do |rental|
+      print "Date: #{rental.date}, "
+      print "Book: #{rental.book.title}, "
+      print "Person: #{rental.person.name?}\n"
+    end
+    print "\n\n"
+  end
+end
